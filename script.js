bdd2936a0685189f87da822221014fd657cbea61
Cristian Bernal <cristianbs@ludik.pe>
2023-09-21 00:47:24 -0500

    FIXED: Style and script

--- script.js
+++ script.js
@@ -1,20 +1,17 @@
 var $barraProgreso = document.querySelector('.bar_progress');
 
-// Funcion para actualizar la barra de progreso
-
-function actualizarBarraProgreso() {
-    // Altura del nevegador
-    const $alturaTotal = document.documentElement.scrollHeight - window.innerHeight;
-
-    // Cuánto se ha desplazado el usuario
+window.addEventListener('scroll', function() {
     const $posicionScroll = window.scrollY;
+
+    const $alturaDocumento = document.documentElement.scrollHeight;
+
+    const $alturaVentana =  window.innerHeight;
+
+    const $alturaTotal =  $alturaDocumento - $alturaVentana;
 
     // Porcentaje de desplazamiento
     const $porcentajeScroll = ($posicionScroll / $alturaTotal) * 100;
 
     // Actualizar el ancho de la barra de progreso
     $barraProgreso.style.width = `${ $porcentajeScroll }%`;
-}
-
-// Agregamos un evento scroll con nuestra funcion para actualizar la barra
-window.addEventListener('scroll', actualizarBarraProgreso);
+});
